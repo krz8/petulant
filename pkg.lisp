682817940a3b2915d22c648b42aafbff7de601eb
Bob Krzaczek <Robert.Krzaczek@gmail.com>
2018-02-23 21:44:23 -0500

    last checkin before starting exp branch

--- pkg.lisp
+++ pkg.lisp
@@ -1,7 +1,12 @@
 (defpackage #:petulant
   (:use #:cl #:anaphora #:iterate #:alexandria)
   (:nicknames #:cli)
-  (:export #:argv #:make-scanner #:scan
-	   #:make-simple #:simple
-	   #:collect
-	   #:spec #:*options* #:*arguments*))
+  (:export
+
+   #:clispec #:name #:flagopt
+   #:style #:partial
+   
+   #:argv #:make-scanner #:scan
+   #:make-simple #:simple
+   #:collect
+   #:spec #:*options* #:*arguments*))
